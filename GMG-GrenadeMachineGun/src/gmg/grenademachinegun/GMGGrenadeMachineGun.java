767a33780ad3c5af5afc3460581dc52aedad2ede
matheg <matheg29@gmail.com>
2016-10-04 09:52:15 +0200

    Prøvd å lage 2 tråder

--- GMG-GrenadeMachineGun/src/gmg/grenademachinegun/GMGGrenadeMachineGun.java
+++ GMG-GrenadeMachineGun/src/gmg/grenademachinegun/GMGGrenadeMachineGun.java
@@ -16,6 +16,8 @@ public class GMGGrenadeMachineGun {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        KeyController keys = new KeyController();
+        ImageAnalyzer img = new ImageAnalyzer();
     }
     
 }
