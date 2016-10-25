f29f21cba5ef2771dfa9c62a3d5f00ac709ffd94
Olav Rune <ornummedal@gmail.com>
2016-10-25 08:41:24 +0200

    Added StorageBox

---
+++ GMG-GrenadeMachineGun/src/gmg/grenademachinegun/StorageBox.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gmg.grenademachinegun;
+
+/**
+ *
+ * @author ib
+ */
+public class StorageBox {
+  private int contents;               // value to be stored
+  private boolean available = false;  // flag
+  
+  private float xError;
+  private float yError;
+ 
+
+  public boolean getAvailable() {
+    return available;
+  }
+  
+  public synchronized int get() {
+    if (available == true) {
+      // value not available, wait for producer
+      available = false;
+    }
+    return contents;
+  }
+
+  // reading value and resetting flag, wake up other threads (producer)
+  public void put(int value) {
+    if (available == false) {
+      contents = value; // store value
+      available = true; // now available for consumer
+    }
+  }
+  
+    public void putError(float x, float y){
+      
+      xError = x;
+      yError = y;
+      
+  }
+}
